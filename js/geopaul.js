92af8756da5f8e730d969bfddabc0be20b20bd89
mccordpa <42620557+mccordpa@users.noreply.github.com>
2023-12-13 19:56:52 -0500

    Update geopaul.js

--- js/geopaul.js
+++ js/geopaul.js
@@ -6,8 +6,8 @@ if($(".selectMenu[data-rel='productsMenuItem']").hasClass("active")){
     //Places Lived Map
     var livedMap = L.map("placesLivedMap").setView([42.000, -81.500], 5);
 
-    L.tileLayer('https://d2munx5tg0hw47.cloudfront.net/tiles/{z}/{x}/{y}.mapbox', {
-        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.{ext}', {
+        attribution: 'MAP tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         subdomains: 'abcd',
         minZoom: 5,
         maxZoom: 11,
